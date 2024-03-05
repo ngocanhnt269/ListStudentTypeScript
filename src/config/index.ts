67a98d5db31e13cf1bd729efd05ee8a25031990b
ngocanhnt269 <ngocanhnt269@gmail.com>
2024-03-05 11:01:33 -0800

    Implemented the student list using Typescript and React

---
+++ src/config/index.ts
@@ -0,0 +1,6 @@
+const Config = {
+    API_BASE_URL: `https://functions4870mze.azurewebsites.net/api/`,
+  };
+  
+  export default Config;
+  
---
+++ src/models/Student.ts
@@ -0,0 +1,13 @@
+export class Student {
+    StudentId: number;
+    FirstName: string;
+    LastName: string;
+    School: string;
+    constructor(StudentId: number, FirstName: string, LastName: string, School: string) {
+      this.StudentId = StudentId;
+      this.FirstName = FirstName;
+      this.LastName = LastName;
+      this.School = School;
+    }
+  }
+  
